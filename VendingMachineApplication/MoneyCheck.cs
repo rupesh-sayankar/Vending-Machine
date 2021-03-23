1fdb21c71b6cdd55fa0f48e51af7f64258de7653
Rupesh Sayankar <44286561+rupesh-sayankar@users.noreply.github.com>
2021-03-23 18:24:39 +0530

    initial commit

---
+++ VendingMachineApplication/MoneyCheck.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachineApplication
+{
+   public class MoneyCheck
+    {
+        bool IsValidCoin;
+        double moneyCount;
+        double TotalMoneyCount;
+
+
+
+    }
+}
